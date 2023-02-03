e3b5233d8b2288d39fe6a3d7bba2b67bc23d2d23
Jeremiah Hansen <jeremiah.hansen@snowflake.com>
2023-02-03 11:02:54 -0700

    Updates to SnowSQL config values

--- utils/snowpark_utils.py
+++ utils/snowpark_utils.py
@@ -58,6 +58,7 @@ def get_snowsql_config(
 
     snowsql_to_snowpark_config_mapping = {
         'account': 'account',
+        'accountname': 'account',
         'username': 'user',
         'password': 'password',
         'rolename': 'role',
