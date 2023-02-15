b4dc0303f0c49fbfff72eec40153125f42d59a65
eplata <domfp13@me.com>
2023-02-15 09:59:00 +0100

    Removing comments in 07_daily_city_metrics

--- steps/07_daily_city_metrics_update_sp/app.py
+++ steps/07_daily_city_metrics_update_sp/app.py
@@ -1,7 +1,6 @@
 #------------------------------------------------------------------------------
 # Hands-On Lab: Data Engineering with Snowpark
 # Script:       07_daily_city_metrics_process_sp/app.py
-# Author:       Jeremiah Hansen, Caleb Baechtold
 # Last Updated: 1/9/2023
 #------------------------------------------------------------------------------
 
