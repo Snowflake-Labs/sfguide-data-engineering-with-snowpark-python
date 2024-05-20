4f8dcc68856b57b666ee4402b6b73c7584bc106a
bohuang <bo.huang@dnb.no>
2024-05-20 22:57:45 +0200

    Add functionality for creating and managing external stage
    This commit adds functions to create an external stage, check if an external stage exists, and create a parquet format. It modifies the load_raw_table function to use these new capabilities and changes a few variable naming for clarity. These changes enhance flexibility and control when working with Snowflake's external stages.

--- steps/02_load_raw.py
+++ steps/02_load_raw.py
@@ -6,7 +6,6 @@ from snowflake.snowpark import Session
 config = toml.load("../.devcontainer/connections.toml")
 default_connection = config["default"]
 
-
 POS_TABLES = ['country', 'franchise', 'location', 'menu', 'truck', 'order_header', 'order_detail']
 CUSTOMER_TABLES = ['customer_loyalty']
 TABLE_DICT = {
@@ -14,22 +13,64 @@ TABLE_DICT = {
     "customer": {"schema": "RAW_CUSTOMER", "tables": CUSTOMER_TABLES}
 }
 
-# SNOWFLAKE ADVANTAGE: Schema detection
-# SNOWFLAKE ADVANTAGE: Data ingestion with COPY
-# SNOWFLAKE ADVANTAGE: Snowflake Tables (not file-based)
 
-def load_raw_table(session, tname=None, s3dir=None, year=None, schema=None):
+def create_parquet_external_stage(session, external_stage_name,
+                                  file_format_name="parquet_format",
+                                  url_to_stage='s3://sfquickstarts/data-engineering-with-snowpark-python/'):
+    try:
+        # Create a file format
+        create_file_format_sql = f"""
+                CREATE OR REPLACE FILE FORMAT {file_format_name} 
+                TYPE = PARQUET
+                COMPRESSION = 'SNAPPY'
+                """
+        # Execute the statement
+        session.sql(create_file_format_sql).collect()
+
+        # Create a stage
+        create_stage_sql = f"""
+                CREATE OR REPLACE STAGE {external_stage_name}
+                FILE_FORMAT = parquet_format
+                URL = '{url_to_stage}';
+                """
+        session.sql(create_stage_sql).collect()
+
+        while not external_stage_exists(session, external_stage_name):
+            print(f"Waiting for external stage {external_stage_name} to be created...")
+            time.sleep(5)
+        print(f"External stage {external_stage_name} created successfully.")
+
+    except Exception as e:
+        print(f"Failed to create external stage {external_stage_name}. Error: {str(e)}")
+
+
+def external_stage_exists(session, stage_name):
+    sql_query = f"""
+            SELECT COUNT(*)
+            FROM INFORMATION_SCHEMA.STAGES
+            WHERE STAGE_NAME = '{stage_name}';
+        """
+    result = session.sql(sql_query).collect()
+
+    return result[0][0] > 0
+
+
+def load_raw_table(session, table_name=None, s3dir=None, year=None, schema=None, stage_name='FROSTBYTE_RAW_STAGE'):
     session.use_schema(schema)
     if year is None:
-        location = "@external.frostbyte_raw_stage/{}/{}".format(s3dir, tname)
+        location = "@external.frostbyte_raw_stage/{}/{}".format(s3dir, table_name)
     else:
-        print('\tLoading year {}'.format(year)) 
-        location = "@external.frostbyte_raw_stage/{}/{}/year={}".format(s3dir, tname, year)
-    
+        print('\tLoading year {}'.format(year))
+        location = "@external.frostbyte_raw_stage/{}/{}/year={}".format(s3dir, table_name, year)
+    if not external_stage_exists(session, stage_name):
+        print(f"Stage {stage_name} does not exist. Creating.")
+        create_parquet_external_stage(session, stage_name)
     # we can infer schema using the parquet read option
-    df = session.read.option("compression", "snappy") \
-                            .parquet(location)
-    df.copy_into_table("{}".format(tname))
+    df = session.read.option("compression", "snappy").parquet(location)
+    df.copy_into_table("{}".format(table_name))
+
+
+# '@"SFTUTORDATAEGNRING"."RAW_POS"."FROSTBYTE_RAW_STAGE"/pos/country/country.snappy.parquet'
 
 # SNOWFLAKE ADVANTAGE: Warehouse elasticity (dynamic scaling)
 
@@ -37,19 +78,21 @@ def load_all_raw_tables(session):
     _ = session.sql("ALTER WAREHOUSE HOL_WH SET WAREHOUSE_SIZE = XLARGE WAIT_FOR_COMPLETION = TRUE").collect()
 
     for s3dir, data in TABLE_DICT.items():
-        tnames = data['tables']
+        table_names = data['tables']
         schema = data['schema']
-        for tname in tnames:
-            print("Loading {}".format(tname))
+        for name in table_names:
+            print("Loading {}".format(name))
             # Only load the first 3 years of data for the order tables at this point
             # We will load the 2022 data later in the lab
-            if tname in ['order_header', 'order_detail']:
+
+            if name in ['order_header', 'order_detail']:
                 for year in ['2019', '2020', '2021']:
-                    load_raw_table(session, tname=tname, s3dir=s3dir, year=year, schema=schema)
+                    load_raw_table(session, table_name=name, s3dir=s3dir, year=year, schema=schema)
             else:
-                load_raw_table(session, tname=tname, s3dir=s3dir, schema=schema)
+                load_raw_table(session, table_name=name, s3dir=s3dir, schema=schema)
 
     _ = session.sql("ALTER WAREHOUSE HOL_WH SET WAREHOUSE_SIZE = XSMALL").collect()
+
 
 def validate_raw_tables(session):
     # check column names from the inferred schema
